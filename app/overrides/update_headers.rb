94719776578921b889f1436533c6d30c9ccfe32e
Maque <daorejuela1@outlook.com>
2017-07-24 12:26:33 +0000

    header changed

---
+++ app/overrides/update_headers.rb
@@ -0,0 +1,9 @@
+Deface::Override.new(:virtual_path => 'spree/shared/_header',
+:name => 'add phone to header',
+:insert_after => 'figure#logo',
+:text => "
+<div class = header_phone>
+Contacto: 000 111 0011
+</div>
+"
+)
