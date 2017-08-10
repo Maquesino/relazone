78e744ca1dbceaebeb22cdb811072a15422f0b11
Maque <daorejuela1@outlook.com>
2017-08-10 20:28:46 +0000

    everythingis working check credit car

---
+++ config/initializers/recaptcha_key.rb
@@ -0,0 +1,4 @@
+     Recaptcha.configure do |config|
+       config.site_key  = '6LfpeCwUAAAAADqihZ-BDPuBYGa8oHgyLxfk185v'
+       config.secret_key = '6LfpeCwUAAAAAOzsmJOu8ft82uFZusDw5qNjBcTT'
+     end
