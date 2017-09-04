f4765256c732ec53c94740ce83eb456422b3ff7a
Maque <daorejuela1@outlook.com>
2017-09-04 20:37:49 +0000

    mejora visual

--- app/overrides/update_footer.rb
+++ app/overrides/update_footer.rb
@@ -10,10 +10,10 @@ Deface::Override.new(:virtual_path => 'spree/layouts/spree_application',
          <div class = col-md-3><h2>Oficina:</h2><h3>Calle siempre viva 123</h3></div>
          <div class = col-md-3><h2>Atención al cliente:</h2><h3>tienda@relazone.com</h3><h3>Tel: 000 11 001110</h3></div>
          <div class = col-md-3><h2>Horario de atención:</h2><h3>Lun-Dom</h3><h3>7:00 a.m - 6:00 p.m</h3></div>
-         <div class = col-md-3 id = social_footnet><h2>¡Siguenos!</h2><h3>Redes sociales:</h3>
-         <br><a href="#" class="fa fa-facebook fa-4x">&nbsp;&nbsp;</a>
-                 <a href="#" class="fa fa-twitter fa-4x">&nbsp;&nbsp;</a>
-              <a href="#" class="fa fa-instagram fa-4x">&nbsp;&nbsp;</a>
+         <div class = col-md-3 id = social_footnet><h2>¡Siguenos!</h2><h3>En nuestras redes sociales:</h3>
+         <br><center ><a href="https://www.facebook.com/RelaStock-481213558903344/"><button type="button" class="footer_text btn btn-primary fa fa-facebook fa-2x">&nbsp;RelaStock</a> <br>
+                 <a href="https://www.facebook.com/RelaStock-481213558903344/"><button type="button" class="footer_text btn btn-info fa fa-twitter fa-2x">&nbsp;@RelaStock</a><br>
+              <a href="https://www.facebook.com/RelaStock-481213558903344/"><button type="button" class="footer_text btn btn-danger fa fa-instagram fa-2x">&nbsp;@RelaStock</a> <br></center>
          </div>
          </div>
          </div>',
